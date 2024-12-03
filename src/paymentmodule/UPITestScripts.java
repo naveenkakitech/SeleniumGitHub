eff89fbf02d2175071bc432ca5f17f80e4f60342
naveenkakitech <naveenkaki5@gmail.com>
2024-12-03 11:41:55 +0530

    Updated Test Class

--- src/paymentmodule/UPITestScripts.java
+++ src/paymentmodule/UPITestScripts.java
@@ -2,18 +2,25 @@ package paymentmodule;
 
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeTest;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterTest;
 
 public class UPITestScripts {
+	WebDriver driver;
   @Test
-  public void f() {
+  public void techlearn() {
+	  driver.get("https://www.techlearn.in");
   }
   @BeforeTest
   public void beforeTest() {
+	  driver = new ChromeDriver();
+	  driver.manage().window().maximize();
   }
 
   @AfterTest
   public void afterTest() {
+	  driver.quit();
   }
 
 }
